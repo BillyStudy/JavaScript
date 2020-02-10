77b22452bd91bf8237b5ad1d74ab2e0244d189a4
BillyCoding <gustavo.rocha.191066@hotmail.com>
2020-02-10 14:55:27 -0300

    Aula 13

---
+++ js/Mod1.js
@@ -0,0 +1,107 @@
+/* [Aula 2] 
+console.log('hello world');*/
+
+/*[Aula 3] 
+var pessoa = {
+    nome: 'Gustavo Ferreira',
+    idade: 18,
+    altura: 1.75
+
+}
+
+console.log(pessoa);
+*/
+
+/* [Aula 4] 
+
+var x = 2;
+var y = 10;
+
+var sum = x + y;
+var sub = x - y;
+var mult = x * y;
+var div = x / y;
+
+console.log('Soma: ' + sum)
+console.log('Sub: ' + sub)
+console.log('Multi: ' + mult)
+console.log('Div: ' + div)*/
+
+/* [Aula 5]
+function Soma(num1, num2){
+    var res = num1 * num2;
+    console.log(res);
+    return res;
+}
+
+Soma(10, 10)*/
+
+/* [Aula 6] 
+function Sexo(sexoAbre){
+
+    var sexo;
+
+    if(sexoAbre === 'M'){
+        sexo = 'Masculino';
+    }else if (sexoAbre === 'F'){
+        sexo = 'Feminino'
+    }else{
+        sexo = 'Outro'
+    }
+    console.log(sexo);
+    return sexoAbre;
+}
+
+function SexoSwitch(sexo){
+    switch(sexo){
+        case 'M': 
+            return 'Masculino';
+        case 'F': 
+            return 'Feminino';
+        default: 
+            return 'Outro';
+    }
+}
+
+console.log(SexoSwitch('F'));*/
+
+/* [Aula 7]
+var nome= 'Gustavo', idade = 18;
+
+if (idade >= 18) {
+    console.log('Voce tem mais de 18 anos')
+}
+*/
+
+/* [Aula 8]
+var idade = 18;
+
+var retorno = (idade >= 18) ? 'Adulto' : 'Crianca'
+
+console.log(retorno);  
+*/
+
+/*[Aula 9]
+
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+} 
+
+var y = 10;
+while (y >= 1) {
+    console.log(y);
+    y--
+}*/
+
+/*[Aula 10] 
+function exibeNome() {
+    console.log('Curso JS');
+}
+
+
+setTimeout(exibeNome, 2000)*/
+
+
+
+
+
---
+++ js/Mod2.js
@@ -0,0 +1,10 @@
+function aula1() {
+    alert('Ativado');
+}
+
+function aula2() {
+    var nome = document.querySelector('input[id=nome]');
+
+    
+    alert(nome.value);
+}
